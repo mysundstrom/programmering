5221c65f0bcfb69dddef50670034af558add7f08
My Sundström <mysundstrom@hotmail.com>
2023-09-25 13:44:13 +0200

    hej

---
+++ 7050 if sats/if else.py
@@ -0,0 +1,15 @@
+tal= int(input("mata in ett tal: "))
+if tal <0:
+    print("talet är ensiffrigt")
+if tal <10:
+    print("talet är tvåsiffrigt")
+if tal <100:
+    print("talet är tresiffrigt")
+if tal <1000:
+    print("talet är minst fyrsiffrigt")
+
+
+
+
+
+
---
+++ 7050 if sats/test 3.py
@@ -0,0 +1,9 @@
+text = input("skriv in en färg")
+if text == "blå":
+    print("flott färg")
+elif text == "gul":
+    print("ful färg")
+elif text == "grön":
+    print("skön färg")
+else:
+    print("fin färg")
